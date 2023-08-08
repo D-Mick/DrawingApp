f1970405de8dbd75adcf108d4ff8be4e58bd72f6
Durotimi <timi@weprototype.co>
2023-08-08 22:02:59 +0100

    Added insightful comments to help understand the code, created a method to set the size of the brush to be proportional to the size of the device

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import com.example.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var drawingView: DrawingView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.drawingView.setSizeForBrush(20.toFloat())
     }
 }
