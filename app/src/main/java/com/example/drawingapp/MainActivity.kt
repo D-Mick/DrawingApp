4cdfb121e600e9561c7c146bcb3b4d1d618be55d
Durotimi <timi@weprototype.co>
2023-08-08 19:02:21 +0100

    Creating project

---
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.drawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drawingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
