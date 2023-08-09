8136abb5609835ceccc58e0aff2cc34408e4b179
Durotimi <timi@weprototype.co>
2023-08-09 11:43:38 +0100

    Created a dialog to allow users to selecet different brush sizes

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.drawingView.setSizeForBrush(20.toFloat())
+
+        binding.ibBrush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    /**
+     * This method is called when you want to change the size of the brush
+     * and sets the brush/eraser sizes to the new values
+     * depending on user selection.
+     */
+    private fun showBrushSizeChooserDialog(){
+        var brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size:")
+
+        val smallBrushBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        smallBrushBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBrushBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        mediumBrushBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBrushBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+        largeBrushBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
